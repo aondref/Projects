8d8d9e370124cabeaf78c324c035ff9264cc2c66
Aondre Franklin <aondref@gmail.com>
2021-12-16 20:15:37 -0500

    playing unique values

--- project4/scripts.js
+++ project4/scripts.js
@@ -1,4 +1,4 @@
-//The Root
+//Playing with .map method
 const root = document.querySelector('#map-method');
 
 const people = [
@@ -26,3 +26,37 @@ const people = [
 
 const names = people.map((person) => `<p>${person.name}, ${person.age}, ${person.position}</p>` )
 root.innerHTML = names.join('');
+
+//Finding unique values
+const menu = [
+    {
+        name: 'pancakes',
+        category: 'breakfast',
+    },
+    {
+        name: 'burger',
+        category: 'lunch',
+    },
+    {
+        name: 'steak',
+        category: 'dinner',
+    },
+    {
+        name: 'bacon',
+        category: 'breakfast',
+    },
+    {
+        name: 'eggs',
+        category: 'breakfast',
+    },
+    {
+        name: 'pasta',
+        category: 'dinner',
+    },
+];
+const categories = ['all', ... new Set (menu.map((item) => item.category))];
+
+const result = document.querySelector('#unique-values');
+result.innerHTML = categories.map((category) => {
+    return `<button>${category}</button>`;
+}).join(' ');
