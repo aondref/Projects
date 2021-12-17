a1a546edbc181f751d5b9bf9535291ed879b4bf8
Aondre Franklin <aondref@gmail.com>
2021-12-16 19:54:27 -0500

    playing with map

--- project4/scripts.js
+++ project4/scripts.js
@@ -1,4 +1,28 @@
 //The Root
-const root = document.querySelector('#root');
+const root = document.querySelector('#map-method');
 
-const people = [];
+const people = [
+    {
+        name: 'bob',
+        age: 20,
+        position: 'developer',
+    },
+    {
+        name: 'anna',
+        age: 25,
+        position: 'designer',
+    },
+    {
+        name: 'susy',
+        age: 30,
+        position: 'the boss',
+    },
+    {
+        name: 'john',
+        age: 20,
+        position: 'intern',
+    },
+];
+
+const names = people.map((person) => `<p>${person.name}, ${person.age}, ${person.position}</p>` )
+root.innerHTML = names.join('');
