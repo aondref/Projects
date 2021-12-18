4fac2040ae886bf3bcca66c4436781c54eb5add8
Aondre Franklin <aondref@gmail.com>
2021-12-18 16:11:31 -0500

    playing with reduce methods

--- project4/scripts.js
+++ project4/scripts.js
@@ -115,3 +115,18 @@
 // console.log(randomPerson);
 
 ////Reduce
+// const people = [
+//     { name: 'bob', age: 20, position: 'developer', salary: 100 },
+//     { name: 'peter', age: 25, position: 'designer', salary: 300 },
+//     { name: 'susy', age: 30, position: 'the boss', salary: 400 },
+//     { name: 'anna', age: 35, position: 'intern', salary: 10 },
+// ];
+
+// const dailyTotal = people.reduce((total, person) => {
+//     total += person.salary;
+//     return total;
+// },0)
+
+// console.log(dailyTotal);
+
+////Reduce Objects
