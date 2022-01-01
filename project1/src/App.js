7e3a3500059b29d0c17ca11a4c7d9f92d8a9e380
Aondre Franklin <aondref@gmail.com>
2022-01-01 12:07:47 -0500

    filter method

--- project1/src/App.js
+++ project1/src/App.js
@@ -26,10 +26,15 @@ function App() {
     ] 
   )
 
+  const deleteTask = (id) => {
+    console.log('delete', id, {...tasks});
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className="container">
       <Header/>
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
